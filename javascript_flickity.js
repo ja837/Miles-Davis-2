4a2a83c6268d921eed8b8fe2d30c8ba11ed11934
ja837 <46791789+ja837@users.noreply.github.com>
2021-12-03 11:33:58 -0500

    Add files via upload

---
+++ javascript_flickity.js
@@ -0,0 +1,9 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'center',
+  contain: true,
+  freeScroll: true,
+  wrapAround: true,
+  draggable: false
+});
